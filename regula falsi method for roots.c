b90ade7786bb44dd3198811fe42d3e77272ace05
Harshit Pandey <harshitpy123@gmail.com>
2021-11-26 17:55:43 +0530

    C ciles

---
+++ regula falsi method for roots.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<math.h>
+
+#define   f(x)   x*log10(x) - 1.2
+
+int main()
+{
+
+	 float x0, x1, x2, f0, f1, f2, e;
+	 int step = 1;
+	 system("cls");
+	 label:
+	 printf("\nEnter two initial guesses:");
+	 scanf("%f%f", &x0, &x1);
+	 printf("Enter tolerable error:");
+	 scanf("%f", &e);
+	 f0 = f(x0);
+	 f1 = f(x1);
+	 if( f0*f1 > 0.0)
+	 {
+		  printf("Incorrect Initial Guesses.\n");
+		  goto label;
+	 }
+	 printf("\n\tStep\t\tx0\t\tx1\t\tx2\t\tf(x2)\n");
+	 printf("\t-------------------------------------------------------------------------\n");
+	 do
+	 {
+		  x2 = x0 - (x0-x1) * f0/(f0-f1);
+		  f2 = f(x2);
+		  printf("\t%d\t\t%f\t%f\t%f\t%f\n",step, x0, x1, x2, f2);
+
+		  if(f0*f2 < 0)
+		  {
+			   x1 = x2;
+			   f1 = f2;
+		  }
+		  else
+		  {
+			   x0 = x2;
+			   f0 = f2;
+		  }
+		  step = step + 1;
+
+	 }while(fabs(f2)>e);
+
+	 printf("\nRoot is: %f", x2);
+	 getch();
+	 return 0;
+}
